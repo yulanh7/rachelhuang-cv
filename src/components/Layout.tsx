9e2941b90b1820db6271b7a30421a0ee21df51d5
Yulan Huang <yulanh7@gmail.com>
2025-07-22 09:17:09 +1000

    style: delete padding in layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="relative">
       <Sidebar />
-      <main className="ml-32 p-6">{children}</main>
+      <main>{children}</main>
     </div>
   );
 }
